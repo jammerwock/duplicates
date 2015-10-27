de7ede398519eca767750c61180b561bb7f04238
Aizen <aizen@meta.ua>
2015-10-27 12:58:49 +0200

    v2.1.0

--- duplicates.php
+++ duplicates.php
@@ -5,12 +5,11 @@ $path = trim(fgets(STDIN));
 if(!is_dir($path)){
     exit('Error: '.$path . ' is not a directory. '. PHP_EOL . 'Exit' . PHP_EOL);
 }
-
-$files = new \Duplicates($path);
+$files = new \DuplicateFinder($path);
 $files->findDuplicates();
 
 
-class Duplicates{
+class DuplicateFinder{
     private $path;
     private $resultFileName;
     private $processUnixHiddenFiles;
@@ -23,8 +22,14 @@ class Duplicates{
     }
 
     public function findDuplicates(){
-        $this->processPath($this->path);
-        $this->outputResult();
+        echo 'Start'.PHP_EOL;
+        $this
+            ->processPath($this->path)
+            ->removeUnique()
+            ->hashCheck()
+            ->removeUnique()
+            ->byteCheck()
+            ->outputResult();
     }
     // private
     private function processPath($path){
@@ -47,16 +52,91 @@ class Duplicates{
         }else{
             echo("Cannot open dir: ".$path.PHP_EOL);
         }
+        return $this;
     }
 
     private function processFile($path){
         echo 'Process '.$path . PHP_EOL;
-        $encode = $this->getFileHash($path);
-        $this->files[$encode][] = $path;
+        $this->files[$this->getFileSize($path)][] = $path;
+    }
+
+    private function removeUnique(){
+        echo 'Removing unique... ';
+        $this->files = array_filter($this->files, function($val){
+            return count($val) > 1;
+        });
+        echo 'Done.'.PHP_EOL;
+        return $this;
+    }
+
+
+    private function hashCheck(){
+        echo 'Hash checking... ';
+        $newFiles = array();
+        foreach($this->files as $files){
+            foreach($files as $file){
+                $newFiles[$this->getFileHash($file)][] = $file;
+            }unset($file);
+        }unset($files);
+        $this->files = $newFiles;
+        unset($newFiles);
+        echo 'Done.'.PHP_EOL;
+        return $this;
+    }
+
+    private function byteCheck(){
+        echo 'Byte checking... ';
+        $res = array();
+        foreach($this->files as $files){
+            foreach ($files as $file1) {
+                $res[$file1][] = $file1;
+                foreach ($files as $file2) {
+                    if($file1 == $file2){
+                        continue;
+                    }
+                    if($this->isFilesIdentical($file1, $file2)){
+                        $res[$file1][] = $file2;
+                    }
+                }
+            }
+        }unset($files);
+
+        foreach($res as $k => &$v){
+            foreach($v as $val){
+                if($val == $k){ continue; }
+                unset($res[$val]);
+            }
+        }unset($k, $v);
+        $this->files = array_values($res);
+        unset($res);
+        echo 'Done.'.PHP_EOL;
+        return $this;
     }
 
     private function getFileHash($path){
         return md5_file($path);
+    }
+
+    private function getFileSize($path){
+        return filesize($path);
+    }
+
+    function isFilesIdentical($file_a, $file_b){
+
+        static $readSize = 4096;
+        $fp_a = fopen($file_a, 'rb');
+        $fp_b = fopen($file_b, 'rb');
+
+        $result = true;
+        while (!feof($fp_a) and !feof($fp_b)) {
+            if (fread($fp_a, $readSize) !== fread($fp_b, $readSize)) {
+                $result = false;
+                break;
+            }
+        }
+        fclose($fp_a);
+        fclose($fp_b);
+        return $result;
     }
 
     private function outputResult(){
@@ -77,9 +157,11 @@ class Duplicates{
             $countFound += count($files);
         }unset($hash, $files, $foundFiles);
 
-        echo 'Done. Found '.$countFound.' duplicates.'.PHP_EOL;
+        echo 'Finish. Found '.$countFound.' duplicates.'.PHP_EOL;
 
-        if(file_put_contents($resultFileName, $result)){
+        if(!$result){
+            echo 'No duplicates was found.'.PHP_EOL;
+        }elseif(file_put_contents($resultFileName, $result)){
             echo 'Result successfully written to file ' . __DIR__ . DIRECTORY_SEPARATOR . $resultFileName . PHP_EOL;
         }else{
             echo 'Error while writing in file ' . __DIR__ . DIRECTORY_SEPARATOR . $resultFileName . PHP_EOL;
